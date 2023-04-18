ec0a28f5abad2cdd3ee36c93438ac88c812a3b80
KOUNIK MAITRA <kounikmaitra2633@gmail.com>
2023-04-18 22:05:52 +0530

    form using formik and yup validation

--- src/Components/StudentDashboard.jsx
+++ src/Components/StudentDashboard.jsx
@@ -1,41 +1,51 @@
 import React from "react";
 import { useState } from "react";
 import { Table, TextInput, Label, Button } from "flowbite-react";
+import { useFormik } from "formik";
+import { studentSchema } from "../Validations/StudentValidation";
+
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  phoneNumber: "",
+  rollNumber: "",
+  cgpa: "",
+};
 
 const StudentDashboard = () => {
   const [students, setStudents] = useState([]); // initializing an empty array of objects
 
-  // defining object for a new student
-  const [newStudent, setNewStudent] = useState({
-    firstName: "",
-    lastName: "",
-    rollNumber: "",
-    cgpa: "",
-  });
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues: initialValues,
+      validationSchema: studentSchema,
+      onSubmit: (values, { resetForm }) => {
+        if (editMode) {
+          let newDataSet = students;
+          for (let index = 0; index <= newDataSet.length; index++) {
+            let currStudent = newDataSet[index];
+            if (currStudent.rollNumber === editStudent.rollNumber) {
+              newDataSet[index].firstName = editStudent.firstName;
+              newDataSet[index].lastName = editStudent.lastName;
+              newDataSet[index].email = editStudent.email;
+              newDataSet[index].phoneNumber = editStudent.phoneNumber;
+              newDataSet[index].cgpa = editStudent.cgpa;
+              break;
+            }
+          }
+
+          setStudents(newDataSet);
+
+          setEditMode(false);
+        } else {
+          setStudents([...students, values]);
+        }
+      },
+    });
 
   const [editMode, setEditMode] = useState(false); // state to determine if the user is in edit mode
   const [editStudent, setEditStudent] = useState({}); // initializing the student to be edited as an empty object
-
-  // function to handle changes in input feilds while filling up the form
-  const handleOnChange = (e) => {
-    const { name, value } = e.target;
-    if (editMode) {
-      setEditStudent({ ...editStudent, [name]: value }); // while editing existing student
-    } else setNewStudent({ ...newStudent, [name]: value }); // while adding a new student
-  };
-
-  //function to add a new student
-  const handleAddStudent = (e) => {
-    e.preventDefault();
-    setStudents([...students, newStudent]);
-    console.log(newStudent);
-    setNewStudent({
-      firstName: "",
-      lastName: "",
-      rollNumber: "",
-      cgpa: "",
-    });
-  };
 
   //function to delete an existing student
   const handleOnDelete = (rollNumber) => {
@@ -43,23 +53,6 @@ const StudentDashboard = () => {
       students.filter((student) => student.rollNumber !== rollNumber)
     );
     console.log("delete student with roll number :", rollNumber);
-  };
-
-  // fucntion to update an existing student
-  const handleUpdateStudent = (e) => {
-    e.preventDefault();
-    let newDataSet = students;
-    for (let index = 0; index <= newDataSet.length; index++) {
-      let currStudent = newDataSet[index];
-      if (currStudent.rollNumber === editStudent.rollNumber) {
-        newDataSet[index].firstName = editStudent.firstName;
-        newDataSet[index].lastName = editStudent.lastName;
-        newDataSet[index].cgpa = editStudent.cgpa;
-        break;
-      }
-    }
-    setStudents(newDataSet);
-    setEditMode(false);
   };
 
   // function to trigger student updation
@@ -71,6 +64,11 @@ const StudentDashboard = () => {
     setEditStudent(currStudent);
   };
 
+  const handleEditChange = (e) => {
+    const { name, value } = e.target;
+    setEditStudent({ ...editStudent, [name]: value }); // while editing existing student
+  };
+
   return (
     <div className='w-screen h-screen flex flex-col justify-center items-center px-8 md:px-28'>
       <h1 className='text-4xl md:text-8xl font-extrabold'>
@@ -80,63 +78,117 @@ const StudentDashboard = () => {
       <div className='container'>
         <form
           className='flex flex-col gap-4 py-8'
-          onSubmit={editMode ? handleUpdateStudent : handleAddStudent}>
-          <div>
-            <div className='mb-2 block'>
-              <Label htmlFor='firstName' value='First Name' />
+          onSubmit={(values) => handleSubmit(values)}>
+          <div className='grid grid-cols-2 gap-4'>
+            <div>
+              <div className='mb-2 block'>
+                <Label htmlFor='firstName' value='First Name' />
+              </div>
+              <TextInput
+                id='firstName'
+                name='firstName'
+                type='text'
+                placeholder='Enter your First Name'
+                onChange={editMode ? handleEditChange : handleChange}
+                onBlur={handleBlur}
+                value={editMode ? editStudent.firstName : values.firstName}
+              />
+              {!editMode && errors.firstName && touched.firstName ? (
+                <p className='text-rose-600 font-semibold'>
+                  {errors.firstName}
+                </p>
+              ) : null}
             </div>
-            <TextInput
-              id='firstName'
-              name='firstName'
-              type='text'
-              placeholder='Enter your First Name'
-              onChange={handleOnChange}
-              value={editMode ? editStudent.firstName : newStudent.firstName}
-              required={true}
-            />
-          </div>
-          <div>
-            <div className='mb-2 block'>
-              <Label htmlFor='lastName' value='Last Name' />
+            <div>
+              <div className='mb-2 block'>
+                <Label htmlFor='lastName' value='Last Name' />
+              </div>
+              <TextInput
+                id='lastName'
+                name='lastName'
+                type='text'
+                placeholder='Enter your Last Name'
+                onChange={editMode ? handleEditChange : handleChange}
+                value={editMode ? editStudent.lastName : values.lastName}
+              />
+              {!editMode && errors.lastName && touched.lastName ? (
+                <p className='text-rose-600 font-semibold'>{errors.lastName}</p>
+              ) : null}
             </div>
-            <TextInput
-              id='lastName'
-              name='lastName'
-              type='text'
-              placeholder='Enter your Last Name'
-              onChange={handleOnChange}
-              value={editMode ? editStudent.lastName : newStudent.lastName}
-              required={true}
-            />
-          </div>
-          <div>
-            <div className='mb-2 block'>
-              <Label htmlFor='rollNumber' value='Roll Number' />
+
+            <div>
+              <div className='mb-2 block'>
+                <Label htmlFor='email' value='Email' />
+              </div>
+              <TextInput
+                id='email'
+                name='email'
+                type='email'
+                placeholder='Enter your email address'
+                onChange={editMode ? handleEditChange : handleChange}
+                onBlur={handleBlur}
+                value={editMode ? editStudent.email : values.email}
+              />
+              {!editMode && errors.email && touched.email ? (
+                <p className='text-rose-600 font-semibold'>{errors.email}</p>
+              ) : null}
             </div>
-            <TextInput
-              id='rollNumber'
-              name='rollNumber'
-              type='text'
-              placeholder='Enter your Roll Number'
-              onChange={handleOnChange}
-              value={editMode ? editStudent.rollNumber : newStudent.rollNumber}
-              required={true}
-              disabled={editMode ? true : false}
-            />
-          </div>
-          <div>
-            <div className='mb-2 block'>
-              <Label htmlFor='cgpa' value='CGPA' />
+            <div>
+              <div className='mb-2 block'>
+                <Label htmlFor='phoneNumber' value='Phone Number' />
+              </div>
+
+              <TextInput
+                id='phoneNumber'
+                name='phoneNumber'
+                type='text'
+                placeholder='Enter your Phone Number'
+                onChange={editMode ? handleEditChange : handleChange}
+                onBlur={handleBlur}
+                value={editMode ? editStudent.phoneNumber : values.phoneNumber}
+              />
+              {!editMode && errors.phoneNumber && touched.phoneNumber ? (
+                <p className='text-rose-600 font-semibold'>
+                  {errors.phoneNumber}
+                </p>
+              ) : null}
             </div>
-            <TextInput
-              id='cgpa'
-              name='cgpa'
-              type='text'
-              placeholder='Enter your CGPA'
-              onChange={handleOnChange}
-              value={editMode ? editStudent.cgpa : newStudent.cgpa}
-              required={true}
-            />
+
+            <div>
+              <div className='mb-2 block'>
+                <Label htmlFor='rollNumber' value='Roll Number' />
+              </div>
+              <TextInput
+                id='rollNumber'
+                name='rollNumber'
+                type='text'
+                placeholder='Enter your Roll Number'
+                onChange={editMode ? handleEditChange : handleChange}
+                onBlur={handleBlur}
+                value={editMode ? editStudent.rollNumber : values.rollNumber}
+                disabled={editMode ? true : false}
+              />
+              {!editMode && errors.rollNumber && touched.rollNumber ? (
+                <p className='text-rose-600 font-bold'>{errors.rollNumber}</p>
+              ) : null}
+            </div>
+            <div>
+              <div className='mb-2 block'>
+                <Label htmlFor='cgpa' value='CGPA' />
+              </div>
+              <TextInput
+                id='cgpa'
+                name='cgpa'
+                type='text'
+                placeholder='Enter your CGPA'
+                onChange={editMode ? handleEditChange : handleChange}
+                onBlur={handleBlur}
+                value={editMode ? editStudent.cgpa : values.cgpa}
+              />
+              {!editMode && errors.cgpa && touched.cgpa ? (
+                <p className='text-rose-600 font-semibold'>{errors.cgpa}</p>
+              ) : null}
+            </div>
           </div>
           <Button
             type='submit'
@@ -149,6 +201,8 @@ const StudentDashboard = () => {
           <Table.Head>
             <Table.HeadCell>First name</Table.HeadCell>
             <Table.HeadCell>Last Name</Table.HeadCell>
+            <Table.HeadCell>Email</Table.HeadCell>
+            <Table.HeadCell>Phone Number</Table.HeadCell>
             <Table.HeadCell>Roll Number</Table.HeadCell>
             <Table.HeadCell>CGPA</Table.HeadCell>
             <Table.HeadCell>
@@ -169,6 +223,12 @@ const StudentDashboard = () => {
                 </Table.Cell>
                 <Table.Cell className='whitespace-nowrap font-medium text-gray-900 dark:text-white'>
                   {Student.lastName}
+                </Table.Cell>
+                <Table.Cell className='whitespace-nowrap font-medium text-gray-900 dark:text-white'>
+                  {Student.email}
+                </Table.Cell>
+                <Table.Cell className='whitespace-nowrap font-medium text-gray-900 dark:text-white'>
+                  {Student.phoneNumber}
                 </Table.Cell>
                 <Table.Cell>{Student.rollNumber}</Table.Cell>
                 <Table.Cell>{Student.cgpa}</Table.Cell>
